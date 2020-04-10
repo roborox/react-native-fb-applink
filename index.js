5f581ceaddb00ee25ecd6e431cd8ae927bb13a3b
Ivan Koryakovtsev <beatzhitta@gmail.com>
2020-04-10 17:21:22 +0300

    rebuild

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_native_1 = require("react-native");
-exports.default = react_native_1.NativeModules.FbApplink;
+exports.default = react_native_1.NativeModules.FBAppLink;
